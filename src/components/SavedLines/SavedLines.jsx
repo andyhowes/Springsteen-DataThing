2dcc57ae5054e6a05812010e1c381b545592e62b
ahowes83 <a_howes83@yahoo.com>
2022-08-02 14:25:35 -0500

    dragging works in savedlines

--- src/components/SavedLines/SavedLines.jsx
+++ src/components/SavedLines/SavedLines.jsx
@@ -7,8 +7,11 @@ import './SavedLines.css';
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
 
-function SavedLines() {
+function SavedLines(props) {
   //const [songs, setSongs] = useState('Songs');   //I don't think this is in play
+  const [savedLines, updateSavedLines] = useState('');
+  const [items, setItems] = useState('');
+
   const dispatch = useDispatch();
   const linesStore = useSelector(store => store.savedLines);
   const userStore = useSelector(store => store.user);
@@ -24,6 +27,15 @@ function SavedLines() {
     }, 1000)
   }, []);
 
+  function handleOnDragEnd(result) {
+    const items = Array.from(linesStore);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    console.log('in handleDragEnd:', result);
+
+    updateSavedLines(items);
+  }
+
   const deleteLine = (line_id) =>{
     let lineToDelete = line_id;
     console.log('checking id in deleteLine', line_id);
@@ -36,21 +48,24 @@ function SavedLines() {
       <h1 className="linesCap">SAVED LINES</h1>
       {linesStore.length === 0 || userStore.length === 0? (<p>...Loading...</p>) : (
       <section><div id="linesBox">
-        <table id="lineList">
-          <thead>
-            <tr>
-              <th>Lyric</th>
-              <th>Rhyme</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          {linesStore.map(line => {
-
-            return (
-              <tbody><tr className="lineSingle" key={(line.id)}><td>{line.fragment_text}</td><td>{line.rhyme}</td><td><button className="deleteButton" onClick={()=>{deleteLine(line.saved_id)}}>⊖</button></td></tr></tbody>
-            )
-          })}
-        </table>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="droppableIdForSavedLinesList">
+            {(provided) => (
+              <ul id="lineList" {...provided.droppableProps} ref={provided.innerRef}>
+                {linesStore.map((line, index) => {
+                  let savedLineKey = line.saved_id + 'a';
+                  return (
+                    <Draggable key={savedLineKey} draggableId={savedLineKey} index={index}>
+                    {(provided) =>
+                    (<div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                    <li><button className="deleteButton" onClick={()=>{deleteLine(line.saved_id)}}>⊖</button>
+                    {line.fragment_text}</li></div>)}</Draggable>
+                  )
+                })}
+            </ul>
+          )}
+            </Droppable>
+          </DragDropContext>
       </div></section>)}
     </div>
   );
